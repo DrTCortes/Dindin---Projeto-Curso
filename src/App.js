b8309fd0261a6d83efe3234e6c2719597cc2f61f
DrTCortes <thiagocortes@outlook.com.br>
2021-12-10 04:47:55 -0300

    Criando Projeto

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import './normalize.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container-header">
+        <h1>Dindin</h1>        
+      </div>
+      <div className="container-body">
+
+        <div className="container-super">
+
+          <div className="container-filters">
+            <h2>filtros</h2>
+          </div>
+
+          <div className="container-resume">
+            <h2>Resumo</h2>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
